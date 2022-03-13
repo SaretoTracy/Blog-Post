980b6918a240abf6ab3c1596ca3e16cf6c0e5def
saretotracy <tracysareto@gmail.com>
2022-03-14 00:20:10 +0300

    registering our database

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,15 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from config import config_options
+from .auth import db
+
 
 
 # define new database
 db = SQLAlchemy()  # database object
 DB_NAME = "blog"  # database name
 
-
+bootstrap = Bootstrap()
 # Initializing application
 app = Flask(__name__)
 
@@ -27,12 +29,18 @@ def create_app(config_name):
     app.register_blueprint(auth, url_prefix='/')
 
     # Initializing Flask Extensions
-    bootstrap = Bootstrap(app)
+    bootstrap.init_app(app)
 
     app.config['SECRET_KEY'] ='no474tghff8735757t5hf75t77t8'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://moringa:kimachas@localhost/blog'
+    db.init_app(app)
 
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
+
+    from .models import User, Blog
+    create_database(app)
+
+    
 
     return app
