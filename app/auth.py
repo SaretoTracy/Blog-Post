ed8a3ef8e0ab6a3207d19030120e6f9578b42b76
saretotracy <tracysareto@gmail.com>
2022-03-14 00:55:26 +0300

    validation

--- app/auth.py
+++ app/auth.py
@@ -10,23 +10,20 @@ def signup():
      # getting info from form
     if request.method == 'POST':
         email = request.form.get('email')
-        first_name = request.form.get('firstName')
+        username = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
         #form validation
         
-        if len(email) < 4:
-            flash('Email is too short')
-        elif len(first_name) < 2:
-            flash('First name must be greater than 1 character.')
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists.', category='error')
+        elif len(email) < 4:
             flash('Email must be greater than 3 characters.', category='error')
-        elif len(firstname) < 2:
-            flash('First name must be greater than 1 character.', category='error')
+        elif len(first_name) < 2:
+            flash('username must be greater than 1 character.', category='error')
         elif password1 != password2:
-            flash('Passwords don\'t match')
-        elif len(password1) < 6:
-            flash('Password must be at least 6 characters.')
             flash('Passwords don\'t match.', category='error')
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
