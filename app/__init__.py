4cd9f97525700d29493198f9814cb7aa6bf4cec6
saretotracy <tracysareto@gmail.com>
2022-03-14 00:57:48 +0300

    add Flask-Login

--- app/__init__.py
+++ app/__init__.py
@@ -2,16 +2,23 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from config import config_options
-
+from flask_login import LoginManager
 
 
 
 # define new database
 db = SQLAlchemy()  # database object
 
+login_manager = LoginManager()
+# provides different security levels and by setting it to strong will monitor the changes in a user's request header and log the user out.
+login_manager.session_protection = 'strong'
+
+
+login_manager.login_view = 'auth.login'
+
+
 bootstrap = Bootstrap()
-# Initializing application
-app = Flask(__name__)
+
 
 
 
@@ -20,6 +27,9 @@ app = Flask(__name__)
 
 
 def create_app(config_name):
+
+    # Initializing application
+    app = Flask(__name__)
     # ....
     # Registering the blueprint
     from .main import main as main_blueprint
