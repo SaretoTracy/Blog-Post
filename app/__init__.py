fc1f45aed4d2987f95849d12003dfed2517525a6
saretotracy <tracysareto@gmail.com>
2022-03-13 21:15:36 +0300

    add auth file and routing

--- app/__init__.py
+++ app/__init__.py
@@ -5,10 +5,13 @@ from flask import Flask
 app = Flask(__name__)
 
 
+
 def create_app():
     # ....
     # Registering the blueprint
     from .main import main as main_blueprint
+    from .auth import auth
     app.register_blueprint(main_blueprint)
+    app.register_blueprint(auth, url_prefix='/')
 
     return app
